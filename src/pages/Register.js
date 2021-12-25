7455f723e810ebcbdd2241d5c303f064398bed5b
Milan Sachani <79278557+Milan-960@users.noreply.github.com>
2021-12-25 21:04:27 +0100

    login and register page has been added 😎

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+  });
+
   return (
     <section>
       <h2>Create An Account</h2>
-      <form></form>
+      <form className="form">
+        <div className="input_container">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" placeholder="Please enter your name" />
+        </div>
+        <div className="input_container">
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="Please enter your Email"
+          />
+        </div>
+        <div className="input_container">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Please enter you password"
+          />
+        </div>
+        <div className="btn_container">
+          <button className="btn">Sign-up</button>
+        </div>
+      </form>
     </section>
   );
 };
