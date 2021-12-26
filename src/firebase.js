f32d7ad6502448f3d383c6558ff00b697b494447
Milan Sachani <79278557+Milan-960@users.noreply.github.com>
2021-12-26 21:34:45 +0100

    firebase congif has been changed

--- src/firebase.js
+++ src/firebase.js
@@ -8,14 +8,24 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   database: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  database: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCPvtjv-oI4E2J7Cc3ZyzAlAPERhYThs1g",
+  authDomain: "chat-app-40e25.firebaseapp.com",
+  database: "https://chat-app-40e25.firebaseio.com",
+  projectId: "chat-app-40e25",
+  storageBucket: "chat-app-40e25.appspot.com",
+  messagingSenderId: "1023335637563",
+  appId: "1:1023335637563:web:89b5a7e163622d8136f21e",
 };
 
 // Initialize Firebase
